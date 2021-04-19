d957005d9e43473db4ced9c42faeb2ad935e9367
valivalter <valivalter@gmail.com>
2021-04-19 14:29:52 +0200

    Update Iron.java

--- src/model/materials/Iron.java
+++ src/model/materials/Iron.java
@@ -2,17 +2,22 @@ package model.materials;
 
 import model.settler.Settler;
 
-/**A vas, mint kibányászható nyersanyag osztálya, nem radioaktív, nem szublimál */
+/** A vas, mint kibányászható nyersanyag osztálya, nem radioaktív, nem szublimál. */
 public class Iron extends Material {
 
-    /**Meghívja a paraméterként kapott settler AddIron() függvényét
-     * @param s a kapott settler
+    /**
+     * Meghívja a paraméterként kapott settler AddIron() függvényét.
+     * @param s a kapott Settler
      */
     @Override
     public void store(Settler s) {
         s.addIron(this);
     }
     
+    /**
+	 * Visszaadja a vas String reprezentációját.
+	 * @return a String
+	 */
     @Override
     public String toString() {
         return "iron ";
