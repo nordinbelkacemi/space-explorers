41238fe5027a0956efe5c332ad25dc079eb04f2e
Nordin Belkacemi <belkaceminordin@gmail.com>
2021-04-27 10:08:55 +0200

    fixed singleton's getInstance method in Game and SelectedSettler

--- src/controllers/Game.java
+++ src/controllers/Game.java
@@ -53,7 +53,7 @@ public class Game {
 		choosableSettlers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
 	}
 
-	public Game getInstance() {
+	public static Game getInstance() {
 		return instance;
 	}
 
