ee89ccdb27620b5d834709c508d1ff9813ab448f
Máté <streitmannmate@protonmail.com>
2021-05-10 01:14:37 +0200

    Lowered music volume

--- src/Program.java
+++ src/Program.java
@@ -3,6 +3,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.swing.SwingUtilities;
 
 import view.GameFrame;
@@ -10,7 +11,7 @@ import view.GameFrame;
 public class Program {
 
 	public static void main(String[] args) {
-		// playSound();
+		playSound();
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -26,6 +27,10 @@ public class Program {
 					Clip clip = AudioSystem.getClip();
 					AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("res/moonmen.wav"));
 					clip.open(inputStream);
+					FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+					float range = gainControl.getMaximum() - gainControl.getMinimum();
+					float gain = (range * 0.5f) + gainControl.getMinimum();
+					gainControl.setValue(gain);
 					clip.loop(Clip.LOOP_CONTINUOUSLY);
 				} catch (Exception e) {
 					e.printStackTrace();
